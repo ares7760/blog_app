80d6668d67563f1019dee8523e250de71493f046
ares7760 <ares7760@yahoo.com>
2017-04-25 14:46:33 +0900

    done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,20 @@
 class PostsController < ApplicationController
+  http_basic_authenticate_with name: "user", password: "secret", except: [:index, :show]
+  
   # GET /posts/new
   def new
-
+    @post = Post.new
   end
 
   # POST /posts
   def create
     @post = Post.new(post_params)
-    @post.save
-    redirect_to @post
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end 
   end
 
   # GET /posts/:id
@@ -19,6 +25,31 @@ class PostsController < ApplicationController
   # GET /posts
   def index
     @posts = Post.all
+  end
+
+  # GET /post/:id/edit
+  def edit
+    @post = Post.find(params[:id]) 
+  end   
+
+  # PATCH /posts/:id
+  # PUT /posts/:id
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /posts/:id
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
   end
 
   private 
